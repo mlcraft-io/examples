df27066ccce409b082d0c2f3407b058479519124
Ivan Fokeev <vanya@vanya.cc>
2024-06-15 21:05:26 +0300

    fix: app

--- vannaai/app.py
+++ vannaai/app.py
@@ -1,15 +1,23 @@
 import os
 from urllib.parse import urlparse
+from typing import Optional
 
+import chainlit as cl
+import chainlit.data as cl_data
+from chainlit.data.sql_alchemy import SQLAlchemyDataLayer
 from vanna.openai import OpenAI_Chat
 from vanna.qdrant import Qdrant_VectorStore
-import chainlit as cl
-from chainlit.input_widget import Select
 
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-CONNECTION_STRING = os.getenv("PG_CONNECTION_STRING")
+PG_CONNECTION_STRING = os.getenv("PG_CONNECTION_STRING")
+CHAINLIT_PG_CONNECTION_STRING = os.getenv(
+    "CHAINLIT_PG_CONNECTION_STRING",
+    "postgresql+asyncpg://chainlit:pg_pass@chainlit-postgres:5432/chainlit",
+)
 QDRANT_URL = os.getenv("QDRANT_URL")
+
+cl_data._data_layer = SQLAlchemyDataLayer(conninfo=CHAINLIT_PG_CONNECTION_STRING)
 
 
 class Vanna(Qdrant_VectorStore, OpenAI_Chat):
@@ -22,16 +30,17 @@ vn = Vanna(
     {
         "url": QDRANT_URL,
         "api_key": OPENAI_API_KEY,
+        "model": "gpt-4o",
     }
 )
 
-url = urlparse(CONNECTION_STRING)
+url = urlparse(PG_CONNECTION_STRING)
 vn.connect_to_postgres(
     host=url.hostname,
     port=url.port,
     user=url.username,
     password=url.password,
-    dbname=url.path.lstrip("/"),
+    dbname=url.path[1:],  # Strip the leading '/' from the path to get the database name
 )
 
 df_information_schema = vn.run_sql("SELECT * FROM INFORMATION_SCHEMA.COLUMNS")
@@ -44,17 +53,26 @@ vn.train(plan=plan)
 ASSISTANT_NAME = "Synmetrix"
 
 
-@cl.step(root=True, language="sql", name=ASSISTANT_NAME)
-async def gen_query(human_query: str):
+def gen_query_recursive(human_query: str, trials: int = 3):
     sql_query = vn.generate_sql(human_query)
+
+    if trials == 0:
+        raise ValueError("Query is not a valid SQL query")
+
+    if not vn.is_sql_valid(sql_query):
+        sql_query = gen_query_recursive(human_query, trials - 1)
+
     return sql_query
 
 
-@cl.step(root=True, name="")
-async def execute_query(query: str):
-    if not vn.is_sql_valid(query):
-        raise ValueError("Query is not a valid SQL query")
+@cl.step(language="sql", name=ASSISTANT_NAME)
+async def gen_query(human_query: str):
+    sql_query = gen_query_recursive(human_query)
+    return sql_query
 
+
+@cl.step(name="")
+async def execute_query(query: str):
     current_step = cl.context.current_step
     df = vn.run_sql(query)
     current_step.output = df.head().to_markdown(index=False)
@@ -72,7 +90,7 @@ async def plot(human_query, sql, df):
     return fig
 
 
-@cl.step(type="run", root=True, name=ASSISTANT_NAME)
+@cl.step(type="run", name=ASSISTANT_NAME)
 async def chain(human_query: str):
     if vn.is_sql_valid(human_query):
         sql_query = human_query
@@ -96,21 +114,9 @@ async def main(message: cl.Message):
     await chain(message.content)
 
 
-@cl.on_chat_start
-async def setup():
-    settings = await cl.ChatSettings(
-        [
-            Select(
-                id="Model",
-                label="OpenAI - Model",
-                values=[
-                    "gpt-4o",
-                    "gpt-4-turbo",
-                    "gpt-4",
-                    "gpt-3.5-turbo",
-                ],
-                initial_index=0,
-            )
-        ]
-    ).send()
-    value = settings["Model"]
+@cl.password_auth_callback
+def auth_callback(username: str, password: str) -> Optional[cl.User]:
+    if (username, password) == ("admin", "admin"):
+        return cl.User(identifier="admin")
+    else:
+        return None
